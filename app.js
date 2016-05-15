b5f8c9e8c1247190c2945530d1892247126ebf7c
MKA1500 <maciejkabsch@gmail.com>
2016-05-15 23:20:08 +0200

    ...

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-	  var app = angular.module('teamWork', []);
-	  
-	  app.controller( 'pracControl', function($scope, $http){
-	    $http.get('contributors.json').success(function(data){
-		
-	data.sort(function(a, b){
-    return b.contributions - a.contributions;
-});
-
-	   $scope.pracownicy = data; });
-	  });
-	  
-	  
-	  app.controller( 'repoControl', function($scope, $http){
-	    $http.get('repositories.json').success(function(data){
-		
-	data.sort(function(a, b){
-    return b.forks - a.forks;
-});
-
-	   $scope.repos = data; });
-	  });
+         $(document).ready(function() {
+			
+            $("#driver").click(function(event){
+               $.getJSON('contributors.json', function(jd) {
+                  $('.tr').html('<td>' + jd[0].nickname + '</td>');
+                  $('.tr').append('<td>' + jd[0].team+ '</td>');
+                  $('.tr').append('<td>' + jd[0].contributions + '</td>');
+               });
+            });
+				
+         });
