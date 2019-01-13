2095c4a40b84faa16cf764d315896bf0b7c93281
MKA1500 <maciejkabsch@gmail.com>
2019-01-13 16:05:59 +0100

    hands on api

--- new-api/index.js
+++ new-api/index.js
@@ -1,2 +1,39 @@
 const express = require('express');
 const app = express();
+let servers = [
+    {
+      name: 'Testserver',
+      capacity: 10,
+      id: 1
+    },
+    {
+      name: 'Liveserver',
+      capacity: 100,
+      id: 2
+    },
+    {
+      name: 'Liveserver 2',
+      capacity: 90,
+      id: 3
+    }
+  ];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/servers', (req, res) => {
+  res.send(servers);
+});
+
+app.get('/api/servers/:id', (req, res) => {
+  const server = servers.find(c => c.id === parseInt(req.params.id));
+  if (!server) {
+    res.status(404).send('The server with a given ID was not found');
+  } else {
+    res.send(server);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
